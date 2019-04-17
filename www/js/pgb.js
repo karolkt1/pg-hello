083ab9c001cff7248e4b741daa408439a9685ffa
karolkt1 <32594640+karolkt1@users.noreply.github.com>
2019-04-17 16:54:13 +0200

    Add files via upload

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,5 +1,16 @@
 var textToRead;
 
+TTS.speak({
+  text: 'Good morning, how are you?',
+  locale: 'en-US',
+  rate: 1
+}, function () {
+  console.log('Text succesfully spoken');
+}, function (reason) {
+  console.log(reason);
+});
+
+
 function popup() {
     textToRead = prompt("Co mam przeczytać", "");
 
