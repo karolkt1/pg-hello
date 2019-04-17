e1b4bfcdf78af587ebaca0306a9435ec1b7f85eb
karolkt1 <32594640+karolkt1@users.noreply.github.com>
2019-04-17 18:00:01 +0200

    Add files via upload

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -13,10 +13,33 @@ function popup() {
     }
   };
   
+function bilboBaggins(){
+  // make sure your the code gets executed only after `deviceready`.
+document.addEventListener('deviceready', function () {
+  // basic usage
+  TTS
+      .speak('hello, world!').then(function () {
+          alert('success');
+      }, function (reason) {
+          alert(reason);
+      });
 
+  // or with more options
+  TTS
+      .speak({
+          text: 'hello, world!',
+          locale: 'en-GB',
+          rate: 0.75
+      }).then(function () {
+          alert('success');
+      }, function (reason) {
+          alert(reason);
+      });
+}, false);
+}
 function speak() {
   speechSynthesis.speak(new SpeechSynthesisUtterance(textToRead));
 };
-function speakOnAndroid(){
+function speakAPI(){
   responsiveVoice.speak(textToRead,"Polish Female");
 };
