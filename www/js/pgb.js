1068861fa8a4c9b8b5ffad46052c6d2d47c9aa2f
karolkt1 <32594640+karolkt1@users.noreply.github.com>
2019-04-17 16:46:54 +0200

    text innej aplikacji

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -1,28 +1,14 @@
-function init() {
-	document.addEventListener("deviceready",onDeviceReady, false);
+var textToRead;
+
+function popup() {
+    textToRead = prompt("Co mam przeczytać", "");
+
+    if (textToRead != null) {
+      document.getElementById("readme").innerHTML =
+      "Zostanie przeczytana następująca wiadomość:" + textToRead;
+    }
+  }
+  
+function speak() {
+  speechSynthesis.speak(new SpeechSynthesisUtterance(textToRead));
 }
-
-function onDeviceReady() {
-	navigator.notification.beep(1);
-}
-
-function deviceInfo() {
-
-	info =  'Hi, I am your smartphone :-)' + '\n' +
-			'=====' + '\n' +
-			'Device Name    : '     + device.name     + '\n' + 
-			'Device Cordova : '  + device.cordova + '\n' + 
-			'Device Platform: ' + device.platform + '\n' + 
-			'Device UUID    : '     + device.uuid     + '\n' + 
-			'Device Model   : '    + device.model     + '\n' + 
-			'Device Version : '  + device.version  + '\n';
-
-	navigator.notification.alert(info);
-	
-}
-function userInfo(){
-	info = "My name is Karol"
-	
-}
-
-
